bf8df874328184d2ace2f6fa3e35a687abbe59b0
vdsilveira <122502607+vdsilveira@users.noreply.github.com>
2023-07-25 22:27:24 -0300

    desafio.go

---
+++ desafio.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+var tempc = k -273
+const k = 373 //ponto de ebulição em kelvin
+
+func main(){
+
+	fmt.Println("a temperatutura de ebulição em graus ºc =", tempc)
+
+
+}
